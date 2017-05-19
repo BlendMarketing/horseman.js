dbe5055a928a5447408a50937cc7b24aaa1d6373
Jared Meyering <jared.meyering@gmail.com>
2017-05-19 00:34:41 -0500

    I'm closer to having good tests. but not close enough

--- src/__tests__/CraftActions-test.js
+++ src/__tests__/CraftActions-test.js
@@ -1,22 +1,32 @@
 /* eslint-env mocha */
+/* global dump */
 
 import { expect } from 'chai';
 import configureStore from 'redux-mock-store';
-import { apiMiddleware } from 'redux-api-middleware';
+import thunk from 'redux-thunk';
+
 import { fetchEntry } from '../actions/craftActions';
 
+
 describe('Craft Actions', () => {
-  const middlewares = [apiMiddleware];
+  const middlewares = [thunk];
   const mockStore = configureStore(middlewares);
 
-  it('should dispatch the proper actions', () => {
+  it('should fetch an entry with a url', () => {
+    const MockFetch = require('mock-fetch-api');
+
+    MockFetch.when('GET', '/api/slug.json')
+      .respondWith(200, JSON.stringify({ hello: 'world' }));
+
     // Initialize mockstore with empty state
-    const initialState = { entries: {} };
+    const initialState = { horsemanEntries: {} };
     const store = mockStore(initialState);
 
     // Dispatch the action
-    store.dispatch(fetchEntry('/api/slug.json'));
+    store.dispatch(fetchEntry('/api/slug.json')).then(() => {
+    });
 
     // @TODO get this test working appropriately
+    expect(store.getActions().length).to.equal(2);
   });
 });
--- src/actions/craftActions.js
+++ src/actions/craftActions.js
@@ -1,4 +1,5 @@
-import { RSAA } from 'redux-api-middleware';
+import 'whatwg-fetch';
+import 'es6-promise/auto';
 
 /**
  * Fetch a singular endpoint from the craft element api and add it to the
@@ -6,33 +7,37 @@ import { RSAA } from 'redux-api-middleware';
  *
  * @param endpoint {string} The endpoint where the entry is located.
  */
-export const fetchEntry = endpoint => ({
-  [RSAA]: {
-    endpoint,
-    method: 'GET',
-    bailout: state => typeof state.horsemanEntries[endpoint] !== 'undefined',
-    types: [
-      { type: '@@horseman/FETCH_ENTRY_REQUEST', meta: { endpoint } },
-      { type: '@@horseman/ADD_CRAFT_ENTRY', meta: { endpoint } },
-      { type: '@@horseman/FETCH_ENTRY_FAIL', meta: { endpoint } },
-    ],
-  },
-});
+export const fetchEntry = endpoint => (dispatch, getState) => {
+  if (typeof getState().horsemanEntries[endpoint] !== 'undefined') {
+    return null;
+  }
+
+  dispatch({ type: '@@horseman/FETCH_ENTRY_REQUEST', meta: { endpoint } });
+
+  return fetch(endpoint)
+  .then(
+    response => response.json()
+    .then(payload => dispatch({ type: '@@horseman/ADD_CRAFT_ENTRY', meta: { endpoint }, payload })),
+  )
+  .catch(() => dispatch({ type: '@@horseman/FETCH_ENTRY_FAIL', meta: { endpoint } }));
+};
 
 /**
  * Fetch multiple horsemanEntries from a singular element api endpoint.
  *
  * @param endpoint {string} The endpoint where the resources are located.
  */
-export const fetchCollection = endpoint => ({
-  [RSAA]: {
-    endpoint,
-    method: 'GET',
-    bailout: state => typeof state.horsemanCollections[endpoint] !== 'undefined',
-    types: [
-      { type: '@@horseman/FETCH_COLLECTION_REQUEST', meta: { endpoint } },
-      { type: '@@horseman/ADD_CRAFT_COLLECTION', meta: { endpoint } },
-      { type: '@@horseman/FETCH_COLLECTION_FAIL', meta: { endpoint } },
-    ],
-  },
-});
+export const fetchCollection = endpoint => (dispatch, getState) => {
+  if (typeof getState().horsemanCollections[endpoint] !== 'undefined') {
+    return null;
+  }
+
+  dispatch({ type: '@@horseman/FETCH_COLLECTION_REQUEST', meta: { endpoint } });
+
+  return fetch(endpoint)
+  .then(
+    response => response.json()
+    .then(payload => dispatch({ type: '@@horseman/ADD_CRAFT_COLLECTION', meta: { endpoint }, payload })),
+  )
+  .catch(() => dispatch({ type: '@@horseman/FETCH_COLLECTION_FAIL', meta: { endpoint } }));
+};
