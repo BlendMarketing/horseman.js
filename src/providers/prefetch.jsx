3648e9da3bb3592ad5c641957f5ebb79dc16ad22
Marc Tanis <marcato15@gmail.com>
2017-11-29 08:34:15 -0600

    save changes

--- src/providers/prefetch.jsx
+++ src/providers/prefetch.jsx
@@ -4,15 +4,18 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import * as types from "../constants/ActionTypes";
 import ActionFactory from "../ActionFactory";
+import LoadUri from "../LoadUri";
 
-const prefetch = (Component, routes) => {
+export default function (Component, routes) {
   class PrefetchLink extends React.Component {
     componentWillMount() {
       this.preloadLink();
     }
+
     preloadLink() {
       const { preload, to } = this.props;
-      preload(resourceUrl);
+      const endpoint = LoadUri(routes, to);
+      preload(endpoint);
     }
 
     render() {
@@ -20,22 +23,24 @@ const prefetch = (Component, routes) => {
     }
   }
 
+  PrefetchLink.propTypes = {
+    /*
+     * The url the link is pointed to
+     */
+    to: PropTypes.string.isRequired,
+    /*
+     * The function in dispatch
+     */
+    preload: PropTypes.func.isRequired,
+  };
   /**
    * The component needs to be able to fetch the resource we want.
    */
   const mapDispatchToProps = dispatch => ({
     preload: uri => dispatch(ActionFactory(types.ADD_RESOURCE)(uri)),
   });
-  const mapStateToProps = (state, ownProps) => {
-    const endpoint = LoadUri(routes, ownProps.to);
-    return {
-      resourceUrl,
-      resource: state.horsemanResources[endpoint] || {
-        meta: { loading: true, error: false },
-        data: {},
-      },
-    };
-  };
+
+  const mapStateToProps = () => {};
 
   return connect(mapStateToProps, mapDispatchToProps)(PrefetchLink);
-};
+}
