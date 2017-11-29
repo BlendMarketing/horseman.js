47cfaae0a3b833991c70ac0699bb8f5eb3b0802f
Jared Meyering <jared.meyering@gmail.com>
2017-11-29 17:37:57 +0000

    export the prefetch component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@
  * @module horseman.js
  */
 import { ConnectedResourceProvider as ResourceProvider } from './providers/ResourceProvider';
+import prefetch from './providers/prefetch';
 import horsemanReducer from './reducers/horsemanReducer';
 
 export {
   horsemanReducer,
+  prefetch,
   ResourceProvider,
 };
